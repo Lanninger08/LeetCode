a59516edc4bf883af45e2c2dee54b19fc380d57f
Lanninger08 <1979383@qq.com>
2021-07-17 17:53:11 +0800

    链表中倒数第k个节点

---
+++ 剑指 Offer（第 2 版）/22.cpp
@@ -0,0 +1,26 @@
+// 剑指 Offer 22. 链表中倒数第k个节点
+// https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+
+#include<stdio.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* getKthFromEnd(ListNode* head, int k) {
+        ListNode* slow = head;
+        for(int i=0;i<k;++i){
+            head = head->next;
+        }
+        while(head){
+            head = head->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
