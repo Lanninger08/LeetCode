b9391c6086d7c13b89d343b7062df7e8bb21f890
Lanninger08 <1979383@qq.com>
2021-08-02 12:20:56 +0800

    数组中出现次数超过一半的数字

---
+++ 剑指 Offer（第 2 版）/39.cpp
@@ -0,0 +1,30 @@
+// 剑指 Offer 39. 数组中出现次数超过一半的数字
+// https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof/
+
+#include<vector>
+using namespace std;
+
+/*
+本题常见的三种解法：
+
+哈希表统计法： 遍历数组 nums ，用 HashMap 统计各数字的数量，即可找出 众数 。此方法时间和空间复杂度均为 O(N) 。
+数组排序法： 将数组 nums 排序，数组中点的元素 一定为众数。
+摩尔投票法： 核心理念为 票数正负抵消 。此方法时间和空间复杂度分别为 O(N) 和 O(1) ，为本题的最佳解法。
+
+*/
+
+class Solution {
+// 摩尔投票法
+public:
+    int majorityElement(vector<int>& nums) {
+        int x = 0, votes = 0, count = 0;
+        for(int num : nums){
+            if(votes == 0) x = num;
+            votes += num == x ? 1 : -1;
+        }
+        // 验证 x 是否为众数
+        for(int num : nums)
+            if(num == x) count++;
+        return count > nums.size() / 2 ? x : 0; // 当无众数时返回 0
+    }
+};
