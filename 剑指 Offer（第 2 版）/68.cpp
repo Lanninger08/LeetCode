f3389e68fa7f98e01a6c67628d715a55ca4af9da
Lanninger08 <1979383@qq.com>
2021-09-05 15:25:35 +0800

    二叉(搜索)树的最近公共祖先

--- 剑指 Offer（第 2 版）/68.cpp
+++ 剑指 Offer（第 2 版）/68.cpp
@@ -24,3 +24,20 @@ public:
         return ancestor;
     }
 };
+
+// 剑指 Offer 68 - II. 二叉树的最近公共祖先
+// https://leetcode-cn.com/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr || root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        // 省略了 if(left == nullptr && right == nullptr) return nullptr;
+        // 不在left，说明在right
+        if(left == nullptr) return right;
+        if(right == nullptr) return left;
+        // if(left != null and right != null) 说明pq在节点的两侧，当前节点为我们要的值
+        return root;
+    }
+};
